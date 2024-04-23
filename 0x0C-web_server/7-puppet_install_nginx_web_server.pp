a69bfced4933520d491ae0c2636ae06e16c92b37
adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-04-23 13:37:44 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,3 @@
-# install nginx, listen on port 80
-# GET with curl return "Hello World"
-# redirection must a 301 Moved permanently
-
 # Ensure Nginx package is installed
 package { 'nginx':
   ensure => installed,
@@ -9,20 +5,21 @@ package { 'nginx':
 
 # Define the Nginx configuration file
 file { '/var/www/html/index.html':
+  ensure  => present,
   content => 'Hello World',
-  path    => '/var/www/html/index.html'
 }
 
-file_line { 'title':
-  ensure   => present,
-  path     => '/etc/nginx/sites-available/default',
-  after    => 'listen 80 default_server;',
-  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  multiple => true
+# Add redirection to Nginx configuration
+file_line { 'nginx_redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  after  => 'listen 80 default_server;',
 }
 
 # Ensure Nginx service is running and enabled
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
