3e985bcd99748ae6df855d859a3568b327a249be
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-20 17:22:26 +0000

    clien_api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Export API data to CSV"""
+
+import csv
+import requests
+import sys
+
+
+def export_tasks_to_csv(user_id):
+    # Construct user URL
+    url_user = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+
+    # Fetch user data
+    user_res = requests.get(url_user)
+    user_data = user_res.json()
+    user_name = user_data.get('username')
+
+    # Fetch tasks data
+    task_res = requests.get(f'{url_user}/todos')
+    tasks = task_res.json()
+
+    # Write data to CSV
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        # Write header
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        # Write tasks
+        for task in tasks:
+            csv_writer.writerow([user_id, user_name, task["completed"], task["title"]])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py USER_ID")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    export_tasks_to_csv(user_id)
