a4b78d4a06e5d31cefc5633a9c5284aca7581857
Adeoye1996 <abdulazeezbabatunde10@gmail.com>
2024-05-20 19:27:08 +0000

    from_api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,10 +12,15 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1].isdigit():
             employee_id = int(sys.argv[1])
-            user_response = requests.get(f'{REST_API}/users/{employee_id}')
+            user_response = requests.get(
+                f'{REST_API}/users/{employee_id}'
+            )
             todos_response = requests.get(f'{REST_API}/todos')
 
-            if user_response.status_code == 200 and todos_response.status_code == 200:
+            if (
+                user_response.status_code == 200 and
+                todos_response.status_code == 200
+            ):
                 user_data = user_response.json()
                 todos_data = todos_response.json()
 
@@ -29,6 +34,7 @@ if __name__ == '__main__':
 
                 for task in completed_tasks:
                     print(f'\t {task.get("title")}')
+
             else:
                 print('Error fetching data from API.')
         else:
